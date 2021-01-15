41d4f14fb096857aab9b50c831fd7332d5d39f31
Alik <alikrugl11@gmail.com>
2021-01-15 14:59:37 +0200

    Version 1

---
+++ src/BitCounting.java
@@ -0,0 +1,12 @@
+public class BitCounting {
+
+    public static int countBits(int n){
+        int count =0;
+        while(n>0){
+            count+=n&1;
+            n>>=1;
+        }
+        return count;
+    }
+
+}
