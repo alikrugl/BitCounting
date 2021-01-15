7f7cd247abdf079aceaa0e6f4fe1166c84ba2389
Alik <alikrugl11@gmail.com>
2021-01-15 15:01:40 +0200

    Version 1

--- src/BitCounting.java
+++ src/BitCounting.java
@@ -1,5 +1,9 @@
 public class BitCounting {
 
+    /**
+     *  Takes a non-negative integer as input,
+     *  and returns the number of bits that are equal to one in the binary representation of that number
+     */
     public static int countBits(int n){
         int count =0;
         while(n>0){
